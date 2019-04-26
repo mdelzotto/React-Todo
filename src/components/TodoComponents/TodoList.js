4d632621ed7e6f9d2c27a9f384827f0544421e4b
mdelzotto <madelzotto@comcast.net>
2019-04-25 22:42:47 -0400

    Finished Todo project with styling and cross out feature

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,18 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react'
+import Todo from './Todo'
+import './Todo.css'
+
+const TodoList = (props) => {
+  return (
+    <div className="TodoList">
+      {props.list.map(item => {
+        return <Todo item={item} key={item.id} reactKey={item.id} addComplete={props.addComplete}/>
+      })}
+    </div>
+  )
+}
+
+export default TodoList
